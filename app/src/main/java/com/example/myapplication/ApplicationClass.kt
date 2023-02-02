619734cc8cb1d949606e7b85abfaae6576d1140f
jinee <sksowk156@gmail.com>
2023-02-02 22:25:03 +0900

    withdrawal

--- app/src/main/java/com/example/myapplication/ApplicationClass.kt
+++ app/src/main/java/com/example/myapplication/ApplicationClass.kt
@@ -2,7 +2,7 @@ package com.example.myapplication
 
 import android.app.Application
 import com.example.myapplication.config.XAccessTokenInterceptor
-import com.example.myapplication.db.local.SharedPreferencesManager
+import com.example.myapplication.util.SharedPreferencesManager
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import com.kakao.sdk.common.KakaoSdk
@@ -13,11 +13,12 @@ import java.util.concurrent.TimeUnit
 
 class ApplicationClass : Application() {
     companion object {
-        const val SERVER_TOKEN: String = "server_token"
-        const val X_ACCESS_TOKEN: String = "x_access_token"         // JWT Token Key
-        const val SHARED_SEARCH_HISTORY = "shared_search_histroy"
-        const val KEY_SEARCH_HISTORY = "key_search_history"
-        const val BASE_URL = "https://pick-cle.shop/"
+        const val SERVER_TOKEN: String = "server_token" // jwt 저장하는 sharedpref를 찾을 때 필요한 key값
+        const val X_ACCESS_TOKEN: String = "x_access_token" // jwt 저장하는 sharedpref에서 jwt 찾을 때 필요한 key값
+        const val SHARED_SEARCH_HISTORY = "shared_search_histroy" // 검색기록을 저장하는 sharedpref를 찾을 때 필요한 key값
+        const val KEY_SEARCH_HISTORY = "key_search_history" // 검색기록을 저장하는 sharedpref에서 검색기록을 찾을 때 필요한 key값
+
+        const val BASE_URL = "https://pick-cle.shop/" // API 의 base_url
 
         lateinit var sharedPreferencesmanager: SharedPreferencesManager // 쉐어드
         lateinit var username: String
